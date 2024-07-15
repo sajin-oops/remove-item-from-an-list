5fe9598754ab216a6d1c0ca81ba07a3078fd4db7
A.K.SAJIN <108419506+sajin-oops@users.noreply.github.com>
2024-07-15 19:07:12 +0530

    Add files via upload

---
+++ remove.py
@@ -0,0 +1,6 @@
+city = ["Chennai","Mumbai","Delhi","Bombay","Hyderabad"]
+print(city)
+#output - ['Chennai', 'Mumbai', 'Delhi', 'Bombay', 'Hyderabad']
+city.remove("Mumbai")
+print(city)
+#output - ['Chennai', 'Delhi', 'Bombay', 'Hyderabad']
